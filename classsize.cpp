0fe00eedff016f7ad77a7f40ee501a236edc47e7
Riya1812 <73591179+Riya1812@users.noreply.github.com>
2020-10-28 17:33:35 +0530

    Created classsize.cpp

---
+++ classsize.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class student
+
+{
+
+    int r;
+
+    char name[14];
+
+}s[6];
+
+int main() {
+
+    printf("%ld", sizeof(s));
+
+    return 0;
+
+}

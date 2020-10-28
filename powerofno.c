343de3e27c085fd30761f8c18672b8df53d50bad
Riya1812 <73591179+Riya1812@users.noreply.github.com>
2020-10-28 18:13:39 +0530

    Created powerofno.c

---
+++ powerofno.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main() 
+
+{
+
+    int a,b;
+
+    scanf("%d%d",&a,&b);
+
+    printf("Square of the number given is:\n");
+
+    printf("%d ^ %d = %.2f\n",a,i,pow(a,b));
+
+    return 0;
+
+}
+
